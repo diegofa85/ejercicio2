1f6561ef55fa924069e2f1ebf1f6a9d6976e8de2
diegofa85 <diegofa85@educastur.es>
2024-11-04 20:25:44 +0100

    ejercicio2-JSX

---
+++ src/Coches.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const coches = [
+    { Matricula: '1425BTY', Marca: 'Peugeot', Modelo: '3008', Tipo: 'híbrido' },
+    { Matricula: '1762MNY', Marca: 'Mercedes', Modelo: 'EQS', Tipo: 'Diesel' },
+    { Matricula: '9882TPK', Marca: 'Renault', Modelo: '4 E-Tech', Tipo: 'Eléctrico' },
+    { Matricula: '6634TRV', Marca: 'Volkswagen', Modelo: 'Golf', Tipo: 'Gasolina' },
+    { Matricula: '2285RPL', Marca: 'Toyota', Modelo: 'RAV4', Tipo: 'híbrido' },
+    { Matricula: '1782PRT', Marca: 'Peugeot', Modelo: '5008', Tipo: 'diesel' },
+  ];
+  
+  export default coches;
